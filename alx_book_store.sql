e7ccdecede843eea490a1069f6bd8fffb17a2f1c
Earnest Achayo <earnytechlive@gmail.com>
2025-02-02 08:44:42 +0300

    create customer, orders, order_details table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -15,3 +15,29 @@ CREATE TABLE IF NOT EXISTS books (
     email VARCHAR(215) UNIQUE NOT NULL,
     address TEXT NOT NULL,
 );
+
+-- Create the customers table
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT NOT NULL
+);
+
+-- Create the orders table
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE
+);
+
+-- Create the order_details table
+CREATE TABLE IF NOT EXISTS order_details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE
+);
